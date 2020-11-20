52f6009d7380386226d0bc942b76b397a58ff288
AnnijaSve <AnnijaSve@inbox.lv>
2020-11-20 19:56:05 +0200

    Added new files

---
+++ app/Stock.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use App\UseCases\TrackStock;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Stock extends Model
+{
+    protected $table = 'stock';
+
+    protected $casts = [
+        'in_stock' => 'boolean'
+    ];
+
+    public function track()
+    {
+        dispatch(new TrackStock($this));
+    }
+
+
+    public function retailer()
+    {
+        return $this->belongsTo(Retailer::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+
+}
---
+++ tests/Unit/ProductHistoryTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Clients\StockStatus;
+use Facades\App\Clients\ClientFactory;
+use App\History;
+use App\Product;
+use App\Stock;
+use Database\Seeders\RetailerWithProductSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ProductHistoryTest extends TestCase
+{
+
+    use RefreshDatabase;
+    /** @test */
+
+    function it_records_history_each_time_stock_is_tracked()
+    {
+        $this->seed(RetailerWithProductSeeder::class);
+
+        $this->mockClientRequest($available = true, $price = 9900);
+
+        $product = tap(Product::first(), function ($product)
+        {
+            $this->assertCount(0, $product->history);
+
+            $product->track();
+
+            $this->assertCount(1, $product->refresh()->history);
+
+        });
+
+
+
+        $history = $product->history->first();
+
+        $this->assertEquals($price, $history->price);
+        $this->assertEquals($available, $history->in_stock);
+        $this->assertEquals($product->id, $history->product_id);
+        $this->assertEquals($product->stock[0]->id, $history->stock_id);
+    }
+}
